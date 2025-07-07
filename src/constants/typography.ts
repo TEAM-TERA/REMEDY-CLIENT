a69ca6d5888ae08b8ce03c3c0f1cf5645950b65f
CraftsManShip001 <seoj080829@gmail.com>
2025-07-08 06:33:30 +0900

    style :: 폰트 추가 및 폰트 상수 지정

---
+++ src/constants/typography.ts
@@ -0,0 +1,59 @@
+import { FONTS } from './fonts';
+
+export const TYPOGRAPHY = {
+    HEADLINE_1: {
+        fontSize: 32,
+        lineHeight: 40,
+        fontFamily: FONTS.BOLD,
+    },
+    HEADLINE_2: {
+        fontSize: 24,
+        lineHeight: 32,
+        fontFamily: FONTS.BOLD,
+    },
+    HEADLINE_3: {
+        fontSize: 20,
+        lineHeight: 26,
+        fontFamily: FONTS.SEMIBOLD,
+    },
+    SUBTITLE: {
+        fontSize: 18,
+        lineHeight: 24,
+        fontFamily: FONTS.MEDIUM,
+    },
+    BODY_1: {
+        fontSize: 16,
+        lineHeight: 22,
+        fontFamily: FONTS.REGULAR,
+    },
+    BODY_2: {
+        fontSize: 16,
+        lineHeight: 22,
+        fontFamily: FONTS.MEDIUM,
+    },
+    CAPTION_1: {
+        fontSize: 14,
+        lineHeight: 20,
+        fontFamily: FONTS.REGULAR,
+    },
+    CAPTION_2: {
+        fontSize: 14,
+        lineHeight: 16,
+        fontFamily: FONTS.LIGHT,
+    },
+    CAPTION_3: {
+        fontSize: 12,
+        lineHeight: 16,
+        fontFamily: FONTS.LIGHT,
+    },
+    BUTTON_TEXT: {
+        fontSize: 16,
+        lineHeight: 22,
+        fontFamily: FONTS.SEMIBOLD,
+    },
+    INPUT_TEXT: {
+        fontSize: 16,
+        lineHeight: 22,
+        fontFamily: FONTS.REGULAR,
+    },
+}
