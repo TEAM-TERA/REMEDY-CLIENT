cf22f8a3c2a584154d21a0e67b66f2ed20565387
sosojung <sojungmoon6@gmail.com>
2025-07-13 05:39:22 +0900

    feat :: 정보 수정 컴포넌트에 필요한 타입 정의

---
+++ src/modules/profile/types/InfoEdit.ts
@@ -0,0 +1,27 @@
+export type GenderType = 'male' | 'female';
+
+export type InfoEditFormData = {
+    name: string;
+    birthDate: string;
+    gender: GenderType;
+};
+
+export type GenderButtonProps = {
+    label: string;
+    isSelected: boolean;
+    onPress: () => void;
+};
+
+export type DateInputProps = {
+    value: string;
+    onChangeText: (text: string) => void;
+    onDatePickerPress: () => void;
+    placeholder?: string;
+};
+
+export type FormInputProps = {
+    label: string;
+    value: string;
+    onChangeText: (text: string) => void;
+    placeholder?: string;
+};
