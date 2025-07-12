10b3c809bb4af8686cce17f95098957a782d930c
sosojung <sojungmoon6@gmail.com>
2025-07-13 04:33:36 +0900

    chore :: 교체 예정 함수 주석 추가

--- src/modules/profile/components/BackButton.tsx
+++ src/modules/profile/components/BackButton.tsx
@@ -9,12 +9,12 @@ type BackButtonProps = {
 };
 
 function BackButton({ label = '', onPress, textStyle = {} }: BackButtonProps) {
-    const handlePress = () => {
-        console.log('Back button pressed');
-    };
+    // 기본 뒤로가기 핸들러 (onPress가 없을 때 사용)
+    const handlePress = () => {};
 
     return (
         <TouchableOpacity onPress={onPress || handlePress}>
+            {/* TODO: navigation.goBack() 또는 navigation.navigate() 로 교체 예정 */}
             <Icon name="chevron-left" size={24} color={TEXT_COLORS.DEFAULT} />
             {label ? <Text style={textStyle}>{label}</Text> : null}
         </TouchableOpacity>
