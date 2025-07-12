7830be9ae5687eff8e95ef1a7b05bf67acae30d9
sosojung <sojungmoon6@gmail.com>
2025-07-13 04:09:34 +0900

    feat :: 프로필 메인 페이지 퍼블리싱 완료

---
+++ src/modules/profile/pages/UserProfileScreen.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import Feather from 'react-native-vector-icons/Feather';
+import { TEXT_COLORS } from '../../../constants/colors';
+import userProfileScreen from '../styles/userProfileScreen';
+import Header from '../components/Header';
+import DropItem from '../components/DropItem';
+import { dropMockData, likeMockData } from '../utils/mockData';
+
+function UserProfileScreen() {
+    const [activeTab, setActiveTab] = useState<'drop' | 'like'>('drop');
+
+    const currentData = activeTab === 'drop' ? dropMockData : likeMockData;
+
+    return (
+        <SafeAreaView style={userProfileScreen.safeAreaView}>
+            <View style={userProfileScreen.container}>
+                <Header
+                    title="프로필"
+                    rightComponent={
+                        <TouchableOpacity>
+                            <SimpleLineIcons
+                                name="settings"
+                                size={20}
+                                color="#fff"
+                            />
+                        </TouchableOpacity>
+                    }
+                />
+                <View style={userProfileScreen.profileContainer}>
+                    <Image 
+                        source={require('../../../assets/images/profileImage.png')}
+                        style={userProfileScreen.profileImage}
+                    />
+                    <View style={userProfileScreen.aliasContainer}>
+                        <Text style={userProfileScreen.aliasText}>모험가</Text>
+                    </View>
+                    <View style={userProfileScreen.profileNameContainer}>
+                        <Text style={userProfileScreen.userNameText}>
+                            User_1
+                        </Text>
+                        <Feather
+                            name="edit"
+                            size={20}
+                            color={TEXT_COLORS.CAPTION}
+                        />
+                    </View>
+                </View>
+
+                <View style={userProfileScreen.tabContainer}>
+                    <View style={userProfileScreen.nav}>
+                        <TouchableOpacity
+                            onPress={() => setActiveTab('drop')}
+                            activeOpacity={1}
+                        >
+                            <Text
+                                style={[
+                                    userProfileScreen.navText,
+                                    activeTab === 'drop' &&
+                                        userProfileScreen.navTextActive,
+                                ]}
+                            >
+                                드랍
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => setActiveTab('like')}
+                            activeOpacity={1}
+                        >
+                            <Text
+                                style={[
+                                    userProfileScreen.navText,
+                                    activeTab === 'like' &&
+                                        userProfileScreen.navTextActive,
+                                ]}
+                            >
+                                좋아요
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    {currentData.map((item, index) => (
+                        <DropItem
+                            key={index}
+                            memo={item.memo}
+                            location={item.location}
+                            imageSource={item.imageSource}
+                            hasHeart={item.hasHeart}
+                        />
+                    ))}
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+}
+export default UserProfileScreen;
