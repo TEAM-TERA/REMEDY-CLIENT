86c3030595f0f6d74998673b7f911c5787cc3588
CraftsManShip001 <seoj080829@gmail.com>
2025-07-14 01:56:07 +0900

    fix :: HeaderNav에 뒤로가기 아이콘 추가

--- src/modules/auth/components/HeaderNav/HeaderNav.tsx
+++ src/modules/auth/components/HeaderNav/HeaderNav.tsx
@@ -1,11 +1,15 @@
 import { Text, View } from "react-native";
 import { styles } from "./styles";
 import { TYPOGRAPHY } from "../../../../constants/typography";
+import BackSvg from "../BackSvg/BackSvg";
 
-function HaderNav(){
+function HeaderNav(){
     return(
         <View style = {styles.container}>
+            <BackSvg></BackSvg>
             <Text style = {[TYPOGRAPHY.SUBTITLE, styles.text]}></Text>
         </View>
     )
 }
+
+export default HeaderNav;
