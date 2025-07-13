a805c27093e58b93b9f3c990b1b7cc8c0c39c09c
CraftsManShip001 <seoj080829@gmail.com>
2025-07-14 01:48:26 +0900

    fix :: BackSvg 스타일 및 색상 추가

---
+++ src/modules/auth/components/BackSvg/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import { rem } from "../../../../utils/scalerRem";
+
+export const styles = StyleSheet.create({
+    container : {
+        width : rem(1.25),
+        height : rem(1.25),
+        flexShrink : 0,
+        aspectRatio: 1/1
+    }
+})
