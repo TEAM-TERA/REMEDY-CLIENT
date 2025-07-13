927c9ab02856b284d9e7742326b01efa53e44052
sosojung <sojungmoon6@gmail.com>
2025-07-14 04:57:16 +0900

    refactor :: 타입 분리, 색상값 상수로 대체, 테스트 코드 삭제

--- src/modules/profile/components/BackButton.tsx
+++ src/modules/profile/components/BackButton.tsx
@@ -1,20 +1,20 @@
-import { TouchableOpacity, Text, GestureResponderEvent } from 'react-native';
-import Icon from 'react-native-vector-icons/Entypo';
-
-type BackButtonProps = {
-    label?: string;
-    onPress?: (event: GestureResponderEvent) => void;
-    textStyle?: object;
-};
+import { TouchableOpacity, Text } from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
+import { BackButtonProps } from '../types/BackButton';
+import { TEXT_COLORS } from '../../../constants/colors';
 
 function BackButton({ label = '', onPress, textStyle = {} }: BackButtonProps) {
     const handlePress = () => {
-        console.log('Back button pressed');
+        // 뒤로가기 네비게이션 구현
     };
 
     return (
         <TouchableOpacity onPress={onPress || handlePress}>
-            <Icon name="chevron-small-left" size={20} color="#fff" />
+            <Entypo
+                name="chevron-small-left"
+                size={24}
+                color={TEXT_COLORS.DEFAULT}
+            />
             {label ? <Text style={textStyle}>{label}</Text> : null}
         </TouchableOpacity>
     );
