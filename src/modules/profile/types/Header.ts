899268f8ce4b4ff53f5d20d56cdb4a9ee69ab4ed
sosojung <sojungmoon6@gmail.com>
2025-07-14 06:07:45 +0900

    refactor :: 타입 분리

---
+++ src/modules/profile/types/Header.ts
@@ -0,0 +1,9 @@
+import { ViewStyle, TextStyle } from 'react-native';
+
+export type HeaderProps = {
+    title: string;
+    onBackPress?: () => void;
+    rightComponent?: React.ReactNode;
+    headerStyle?: ViewStyle;
+    titleStyle?: TextStyle;
+};
