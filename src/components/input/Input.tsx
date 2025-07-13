9ee2d6019d2a79142852ec2e5e0f2004d403db72
CraftsManShip001 <seoj080829@gmail.com>
2025-07-14 05:29:04 +0900

    fix :: button props width 추가

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -4,13 +4,17 @@ import { styles } from "./styles";
 import { TYPOGRAPHY } from "../../constants/typography";
 import { TEXT_COLORS } from "../../constants/colors";
 
-function Input({placeholder, value, onChangeText} : InputProps){
+function Input({placeholder, value, onChangeText, width} : InputProps){
     return(
         <View style = {styles.container}>
             <TextInput
             value = {value}
             onChangeText = {onChangeText}
-            style = {[TYPOGRAPHY.INPUT_TEXT, styles.text]}
+            style = {[
+                TYPOGRAPHY.INPUT_TEXT, 
+                styles.text,
+                width !== undefined ? { width } : null
+            ]}
             placeholder = {placeholder}
             placeholderTextColor={TEXT_COLORS.CAPTION}/>
         </View>
