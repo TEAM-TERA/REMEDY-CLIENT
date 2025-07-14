1dad58af26673f3d11230492f747158ac1fa9acb
CraftsManShip001 <seoj080829@gmail.com>
2025-07-15 04:57:42 +0900

    fix :: App 복구

--- App.tsx
+++ App.tsx
@@ -1,14 +1,6 @@
-// import React from 'react';
-// import RootNavigation from './src/navigation';
-
-// export default function App() {
-//   return <RootNavigation />;
-// }
-
 import React from 'react';
 import RootNavigation from './src/navigation';
-import DropSearchScreen from './src/modules/drop/pages/DropSearchScreen';
 
 export default function App() {
-  return <DropSearchScreen></DropSearchScreen>;
+  return <RootNavigation />;
 }
