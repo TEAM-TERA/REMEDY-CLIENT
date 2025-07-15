da7f4b13afac34473804a81b744c8e1a5a254476
CraftsManShip001 <seoj080829@gmail.com>
2025-07-15 10:27:06 +0900

    chore :: react-native-community 설치

--- src/components/playBar/PlayBar.tsx
+++ src/components/playBar/PlayBar.tsx
@@ -1,8 +1,15 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+import { TYPOGRAPHY } from "../../constants/typography";
+import Slider from "@react-native-community/slider";
+import PlaySvg from "./PlaySvg";
 
-function PlayBar(){
+function PlayBar({currentTime, }){
     return(
-        <View></View>
+        <View style = {styles.container}>
+            <PlaySvg></PlaySvg>
+            <Text style = {[TYPOGRAPHY.CAPTION_3, styles.timeText]}>{formatTime(currentTime)}</Text>
+        </View>
     )
 }
 
--- src/components/playBar/styles.ts
+++ src/components/playBar/styles.ts
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native";
+import { TEXT_COLORS } from "../../constants/colors";
 import { scale } from "../../utils/scalers";
 
 export const styles = StyleSheet.create({
@@ -10,5 +11,8 @@ export const styles = StyleSheet.create({
         gap : scale(12),
         alignSelf : "stretch"
     },
-    
+    timeText : {
+        color : TEXT_COLORS.DEFAULT
+    }
+
 })
