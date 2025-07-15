86ef3a0639d782ece8b402069d38789a2cccada2
CraftsManShip001 <seoj080829@gmail.com>
2025-07-16 03:11:32 +0900

    fix :: CD안에 이미지 넣을 수 있도록 수정

--- src/modules/drop/components/CdPlayer/CdPlayer.tsx
+++ src/modules/drop/components/CdPlayer/CdPlayer.tsx
@@ -7,7 +7,7 @@ import DiscSvg from "./DiscSvg";
 function CdPlayer(){
     return(
         <View style = {styles.container}>
-            <DiscSvg></DiscSvg>
+            <DiscSvg imageUrl="https://water-icon-dc4.notion.site/image/attachment%3Ac6210c58-7309-41bd-8aba-c2725c761d68%3Aimage.png?table=block&id=2312845a-0c9f-8072-9ae3-ce72c6b7b598&spaceId=f74ce79a-507a-45d0-8a14-248ea481b327&width=1420&userId=&cache=v2"></DiscSvg>
             <CircleSvg></CircleSvg>
             <CircleBlurSvg></CircleBlurSvg>
         </View>
