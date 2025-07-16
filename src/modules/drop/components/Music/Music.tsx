8fac108933a37ce2db7b29daf2c1c40921985eee
CraftsManShip001 <seoj080829@gmail.com>
2025-07-17 02:33:33 +0900

    feat :: imgUrl props 추가

--- src/modules/drop/components/Music/Music.tsx
+++ src/modules/drop/components/Music/Music.tsx
@@ -3,13 +3,13 @@ import { styles } from "./styles";
 import { MusicProps } from "../../types/Music";
 import { TYPOGRAPHY } from "../../../../constants/typography";
 
-function Music({ musicTitle, singer, onPress }: MusicProps) {
+function Music({ musicTitle, singer, onPress, imgUrl }: MusicProps) {
   return (
     <TouchableOpacity style={styles.container}
     onPress={onPress}>
       <Image
         source={{
-          uri: "https://water-icon-dc4.notion.site/image/attachment%3A399b6f60-5f40-4b12-a738-fc2a9fc89758%3Aimage.png?table=block&id=22f2845a-0c9f-80e3-a337-c6043069abf5&spaceId=f74ce79a-507a-45d0-8a14-248ea481b327&width=200&userId=&cache=v2",
+          uri: imgUrl,
         }}
         style={styles.imgContainer}
       />
