8b108266b5596d259718c96d86f846013d2275fd
sosojung <sojungmoon6@gmail.com>
2025-07-17 06:44:41 +0900

    feat :: 프로필 페이지 네비게이션 설정

--- src/modules/profile/pages/UserProfileScreen.tsx
+++ src/modules/profile/pages/UserProfileScreen.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+import type { NavigationProp } from '@react-navigation/native';
+import type { ProfileStackParamList } from '../../../types/navigation';
 import { TEXT_COLORS } from '../../../constants/colors';
 import userProfileScreen from '../styles/userProfileScreen';
 import Header from '../components/Header';
@@ -9,12 +12,17 @@ import { dropMockData, likeMockData } from '../utils/mockData';
 import Icon from '../../../components/icon/Icon';
 
 function UserProfileScreen() {
+    const navigation = useNavigation<NavigationProp<ProfileStackParamList>>();
     const [activeTab, setActiveTab] = useState<'drop' | 'like'>('drop');
 
     const currentData = activeTab === 'drop' ? dropMockData : likeMockData;
 
     const handleEditPress = () => {
-        // TODO: NameEditScreen으로 네비게이션
+        navigation.navigate('NameEdit');
+    };
+
+    const handleSettingPress = () => {
+        navigation.navigate('Setting');
     };
 
     return (
@@ -23,7 +31,7 @@ function UserProfileScreen() {
                 <Header
                     title="프로필"
                     rightComponent={
-                        <TouchableOpacity>
+                        <TouchableOpacity onPress={handleSettingPress}>
                             <Icon
                                 name="setting"
                                 width={20}
