0b9a19008e7ebc3a4005adfa784e7cc53c2c6a9d
CraftsManShip001 <seoj080829@gmail.com>
2025-07-17 00:26:22 +0900

    fix :: app 복구

--- App.tsx
+++ App.tsx
@@ -1,14 +1,6 @@
-// import React from 'react';
-// import RootNavigation from './src/navigation';
-
-// export default function App() {
-//   return <RootNavigation />;
-// }
-
 import React from 'react';
 import RootNavigation from './src/navigation';
-import LoginScreen from './src/modules/auth/pages/LoginScreen';
 
 export default function App() {
-  return <LoginScreen></LoginScreen>;
+  return <RootNavigation />;
 }
