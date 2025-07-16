ce352e865410b161b67e56cf7bcef4adfbf35267
CraftsManShip001 <seoj080829@gmail.com>
2025-07-17 02:09:33 +0900

    feat :: 요청 인터셉터로 자동으로 Authorization 헤더 자동 처리

--- src/modules/auth/api/axiosInstance.ts
+++ src/modules/auth/api/axiosInstance.ts
@@ -1,11 +1,22 @@
 import axios from "axios";
 import Config from 'react-native-config';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const API_BASE_URL = Config.API_BASE_URL;
 
 const axiosInstance = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: process.env.API_BASE_URL,
   withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(async (config) => {
+  const token = await AsyncStorage.getItem("userToken");
+  if (token && config.headers) {
+    config.headers.set
+      ? config.headers.set("Authorization", `Bearer ${token}`)
+      : (config.headers["Authorization"] = `Bearer ${token}`);
+  }
+  return config;
 });
 
 export default axiosInstance;
