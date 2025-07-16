57f6062527e1a67054698deed2a9a52b26a8c355
CraftsManShip001 <seoj080829@gmail.com>
2025-07-17 03:45:04 +0900

    fix :: SongSearchItem type 지정

--- src/modules/drop/hooks/useSongSearch.ts
+++ src/modules/drop/hooks/useSongSearch.ts
@@ -1,8 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { searchSongs } from "../api/dropApi";
 
-export function useSongSearch(query: string) {
-  return useQuery({
+export interface SongSearchItem {
+    id: string;
+    title: string;
+    artist: string;
+    duration: number;
+}
+
+export function useSongSearch(query : string) {
+  return useQuery<SongSearchItem[]>({
     queryKey: ["songSearch", query],
     queryFn: () => searchSongs(query),
     enabled: !!query, 
