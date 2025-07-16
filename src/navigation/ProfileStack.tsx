d928d4b31b2ad3f63f3543f3c1602e69259f3546
sosojung <sojungmoon6@gmail.com>
2025-07-17 06:47:15 +0900

    fix :: 프로필 화면에서 기본 헤더 보이는 문제 해결

--- src/navigation/ProfileStack.tsx
+++ src/navigation/ProfileStack.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import type { ProfileStackParamList } from "../types/navigation";
 
 import UserProfileScreen from "../modules/profile/pages/UserProfileScreen";
 import NameEditScreen from "../modules/profile/pages/NameEditScreen";
 import SettingScreen from "../modules/profile/pages/SettingScreen";
 import InfoEditScreen from "../modules/profile/pages/InfoEditScreen";
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<ProfileStackParamList>();
 
 function ProfileStack() {
   return (
-    <Stack.Navigator initialRouteName="UserProfile">
+    <Stack.Navigator initialRouteName="UserProfile" screenOptions={{ headerShown: false }}>
       <Stack.Screen name = "UserProfile" component={UserProfileScreen} />
       <Stack.Screen name = "NameEdit" component={NameEditScreen} />
       <Stack.Screen name = "Setting" component={SettingScreen} />
