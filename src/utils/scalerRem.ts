9abdec11e70344dcd3084d15b5af82b9751ff2bc
CraftsManShip001 <seoj080829@gmail.com>
2025-07-17 06:31:04 +0900

    fix :: 복구 작업

--- src/utils/scalerRem.ts
+++ src/utils/scalerRem.ts
@@ -3,20 +3,3 @@ import { Dimensions, PixelRatio } from 'react-native';
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
 const BASE_WIDTH = 375;
 const BASE_HEIGHT = 812;
-
-<<<<<<< HEAD
-const scale = SCREEN_WIDTH / BASE_WIDTH;
-
-// 폰트 크기 기반 rem 계산 (1rem = 16px)
-const BASE_FONT_SIZE = 16;
-
-// 유사한 rem을 구해줍니다!
-export function rem(size : number) {
-  return Math.round(PixelRatio.roundToNearestPixel(size * BASE_FONT_SIZE * scale));
-}
-=======
-export const scale = (size: number) => (SCREEN_WIDTH / BASE_WIDTH) * size;
-export const verticalScale = (size: number) => (SCREEN_HEIGHT / BASE_HEIGHT) * size;
-export const moderateScale = (size: number, factor = 0.5) =>
-  size + (scale(size) - size) * factor;
->>>>>>> 2c9ebdcb6dd6aee356952caedcae13a79b5fcbc2
