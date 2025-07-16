30a1aca570f0772f0f08f34890eeaee132564bbb
CraftsManShip001 <seoj080829@gmail.com>
2025-07-16 16:10:23 +0900

    feat :: useLogin hook 제작

--- src/modules/auth/api/axiosInstance.ts
+++ src/modules/auth/api/axiosInstance.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "http://localhost:8080/api/v1",
   withCredentials: true,
-  // ...공통 헤더, 인터셉터 등
 });
 
 export default axiosInstance;
