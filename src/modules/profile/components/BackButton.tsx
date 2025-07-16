7e84648356106413aa45a72c76d2863ae3abd806
sosojung <sojungmoon6@gmail.com>
2025-07-17 06:42:49 +0900

    feat :: 뒤로가기 네비게이션 설정

--- src/modules/profile/components/BackButton.tsx
+++ src/modules/profile/components/BackButton.tsx
@@ -1,14 +1,13 @@
 import { TouchableOpacity, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { BackButtonProps } from '../types/BackButton';
 import Icon from '../../../components/icon/Icon';
 
 function BackButton({ label = '', onPress, textStyle = {} }: BackButtonProps) {
-    const handlePress = () => {
-        // 뒤로가기 네비게이션 구현
-    };
+    const navigation = useNavigation();
 
     return (
-        <TouchableOpacity onPress={onPress || handlePress}>
+        <TouchableOpacity onPress={onPress || navigation.goBack}>
             <Icon name="left" width={24} height={24} />
             {label ? <Text style={textStyle}>{label}</Text> : null}
         </TouchableOpacity>
