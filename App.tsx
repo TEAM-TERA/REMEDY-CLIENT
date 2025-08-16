c763aa9a228bd57dcb1fa23a3b90a659262a131c
CraftsManShip001 <seoj080829@gmail.com>
2025-08-16 21:13:40 +0900

    fix :: build 에러 해결

--- App.tsx
+++ App.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import RootNavigation from './src/navigation';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
+
 export default function App() {
-  return <RootNavigation />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RootNavigation />
+    </QueryClientProvider>
+  );
 }
--- src/modules/home/styles/MainFunction/MusicWheel.ts
+++ src/modules/home/styles/MainFunction/MusicWheel.ts
@@ -1,12 +1,12 @@
 import { StyleSheet } from "react-native";
-import { rem } from "../../../../utils/scalerRem";
+import { scale } from "../../../../utils/scalers";
 
 export const styles = StyleSheet.create({
     container : {
-        width : rem(19.625),
-        height: rem(21.375),
+        width : scale(19.625),
+        height: scale(21.375),
         position : "absolute", 
-        right : rem(-7.5),
-        bottom : rem(-7.5)
+        right : scale(-7.5),
+        bottom : scale(-7.5)
     }
 })
