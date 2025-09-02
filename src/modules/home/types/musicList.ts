2b28d8efcea3d3dae6454154cceaf312075c9d5c
sosojung <sojungmoon6@gmail.com>
2025-09-02 22:52:12 +0900

    fix :: 드랍핑 타입 가수와 이미지 소스 속성 추가

--- src/modules/home/types/musicList.ts
+++ src/modules/home/types/musicList.ts
@@ -1,28 +1,32 @@
+import { ImageSourcePropType } from 'react-native';
+
 export interface Dropping {
-  droppingId : string;
-  userId : number;
-  songId : string;
-  title : string;
-  latitude : number;
-  longitude : number;
-  address : string;
+    droppingId: string;
+    userId: number;
+    songId: string;
+    title: string;
+    singer: string;
+    latitude: number;
+    longitude: number;
+    address: string;
+    imageSource: ImageSourcePropType;
 }
 
 export interface NodePosition {
-  angle: number;        // 노드 위치 각도
-  isMain: boolean;      // 메인 노드 여부
-  scale: number;        // 크기 비율
-  opacity: number;      // 투명도
+    angle: number; // 노드 위치 각도
+    isMain: boolean; // 메인 노드 여부
+    scale: number; // 크기 비율
+    opacity: number; // 투명도
 }
 
 export interface VisibleNode {
-  position: NodePosition;
-  song: Dropping;
-  slotIndex: number;    // 슬롯 번호 (0,1,2)
+    position: NodePosition;
+    song: Dropping;
+    slotIndex: number; // 슬롯 번호 (0,1,2)
 }
 
 export interface MusicWheelState {
-  droppings: Dropping[];
-  currentIndex: number;
-  rotation: number;
+    droppings: Dropping[];
+    currentIndex: number;
+    rotation: number;
 }
