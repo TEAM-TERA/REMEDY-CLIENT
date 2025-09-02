5c2d9d512ed151c58196bde37adb351f9bc09237
CraftsManShip001 <seoj080829@gmail.com>
2025-09-03 06:42:43 +0900

    fix :: homeScreen 3개 컴포넌트 결합

--- src/modules/home/pages/HomeScreen.tsx
+++ src/modules/home/pages/HomeScreen.tsx
@@ -1,13 +1,21 @@
-import { SafeAreaView, Text, View } from "react-native";
-import { styles } from "../styles/HomeScreen";
-import MainFunction from "../components/MainFunction";
-import GoogleMapView from "../../../components/map/GoogleMapView";
+import { View } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
 import HeaderBar from "../components/HeaderBar";
+import GoogleMapView from "../../../components/map/GoogleMapView";
+import MusicWheel from "../components/MainFunction/MusicWheel";
 
-function HomeScreen(){
-    return(
-        <GoogleMapView></GoogleMapView>
-    )
+function HomeScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }} edges={['top']}>
+        <View style={{ flex: 1, position: 'relative' }}>
+            <HeaderBar/>
+            <View style={{ flex: 1 }}>
+                <GoogleMapView/>
+            </View>
+            <MusicWheel/>
+        </View>
+    </SafeAreaView>
+
+  );
 }
-
 export default HomeScreen;
