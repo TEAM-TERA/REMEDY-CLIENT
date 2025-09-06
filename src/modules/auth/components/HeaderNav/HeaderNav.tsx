8e20b11d4198e3627c60acc55ec629614b0ffbc0
CraftsManShip001 <seoj080829@gmail.com>
2025-09-06 20:52:10 +0900

    feat :: Blank 컴포넌트 추가

--- src/modules/auth/components/HeaderNav/HeaderNav.tsx
+++ src/modules/auth/components/HeaderNav/HeaderNav.tsx
@@ -2,13 +2,14 @@ import { Text, View } from "react-native";
 import { styles } from "./styles";
 import { TYPOGRAPHY } from "../../../../constants/typography";
 import BackSvg from "../BackSvg/BackSvg";
+import Blank from "../Blank/Blank";
 
 function HeaderNav(){
     return(
         <View style = {styles.container}>
             <BackSvg></BackSvg>
             <Text style = {[TYPOGRAPHY.SUBTITLE, styles.text]}>로그인</Text>
-            <BackSvg></BackSvg>
+            <Blank></Blank>
         </View>
     )
 }
