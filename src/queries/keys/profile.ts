f801280e8d3d8e74198a86ee085bd1004310b518
CraftsManShip001 <seoj080829@gmail.com>
2025-09-07 02:01:14 +0900

    fix :: profileKeys에 detail 추가

--- src/queries/keys/profile.ts
+++ src/queries/keys/profile.ts
@@ -1,4 +1,5 @@
 export const profileKeys = {
     all: ['profile'] as const,
     me: () => [...profileKeys.all, 'me'] as const,
+    detail: (id: string | number) => [...profileKeys.all, 'detail', id] as const,
 };
