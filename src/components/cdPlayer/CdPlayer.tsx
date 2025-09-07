0ce7a3bb2acc1bb7e0ad7bc6167d49974d8f3ccb
sosojung <sojungmoon6@gmail.com>
2025-09-08 06:35:14 +0900

    fix :: cd 플레이어 공통 컴포넌트로 분리

--- src/components/cdPlayer/CdPlayer.tsx
+++ src/components/cdPlayer/CdPlayer.tsx
@@ -1,9 +1,9 @@
-import { View } from "react-native";
-import { useEffect, useRef, useState } from "react";
-import { styles } from "./styles";
-import CircleSvg from "../CircleSvg/CircleSvg";
-import CircleBlurSvg from "../CircleSvg/CircleBlurSvg";
-import DiscSvg from "./DiscSvg";
+import { View } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { styles } from './styles';
+import CircleSvg from '../circleSvg/CircleSvg';
+import CircleBlurSvg from '../circleSvg/CircleBlurSvg';
+import DiscSvg from './DiscSvg';
 
 function CdPlayer() {
     const [tilt, setTilt] = useState(0);
@@ -21,15 +21,15 @@ function CdPlayer() {
     }, []);
 
     return (
-        <View style = {styles.container}>
+        <View style={styles.container}>
             <DiscSvg
-                imageUrl = "https://entertainimg.kbsmedia.co.kr/cms/uploads/CONTENTS_20230425095757_b457a570577d7444e7cef5c0a6e73bd7.png"
-                tilt = {tilt}
+                imageUrl="https://entertainimg.kbsmedia.co.kr/cms/uploads/CONTENTS_20230425095757_b457a570577d7444e7cef5c0a6e73bd7.png"
+                tilt={tilt}
             />
             <CircleSvg />
             <CircleBlurSvg />
         </View>
-    )
+    );
 }
 
 export default CdPlayer;
---
+++ src/components/cdPlayer/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+import { scale } from '../../utils/scalers';
+
+export const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        height: scale(240),
+        padding: scale(10),
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: scale(10),
+    },
+});
