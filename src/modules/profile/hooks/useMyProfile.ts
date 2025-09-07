470d5f69dd34f570140588a9fd89a1a5ec31c82f
CraftsManShip001 <seoj080829@gmail.com>
2025-09-08 08:25:31 +0900

    fix :: useMyProfile에서 토큰 검증 추가

--- src/modules/profile/hooks/useMyProfile.ts
+++ src/modules/profile/hooks/useMyProfile.ts
@@ -1,10 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { getMyProfile } from '../api/profileApi';
 import { profileKeys } from '../../../queries/keys/profile';
+import { useContext } from 'react';
+import { AuthContext } from '../../auth/auth-context';
 
 export function useMyProfile() {
-  return useQuery({
-    queryKey: profileKeys.me(),
-    queryFn: getMyProfile,
-  });
+    const userToken = useContext(AuthContext);
+
+    return useQuery({
+        queryKey: profileKeys.me(),
+        queryFn: getMyProfile,
+        enabled: !!userToken,
+    });
 }
