ce02705597ea7af15dd6ea25439685db7fbbd1cb
CraftsManShip001 <seoj080829@gmail.com>
2025-09-08 14:12:17 +0900

    fix :: userToken만 구조 분해

--- src/modules/profile/hooks/useMyProfile.ts
+++ src/modules/profile/hooks/useMyProfile.ts
@@ -5,7 +5,7 @@ import { useContext } from 'react';
 import { AuthContext } from '../../auth/auth-context';
 
 export function useMyProfile() {
-    const userToken = useContext(AuthContext);
+    const { userToken } = useContext(AuthContext);
 
     return useQuery({
         queryKey: profileKeys.me(),
