45ccaaa27c67476a09e3dd34797e263d303dffc7
sosojung <sojungmoon6@gmail.com>
2025-09-16 23:04:25 +0900

    feat: 도전과제 페이지 퍼블리싱 완료

---
+++ src/modules/challenge/datas/challengeData.ts
@@ -0,0 +1,49 @@
+import { ChallengeData } from '../types/ChallengeProps';
+
+export const dailyChallengeData: ChallengeData[] = [
+    {
+        id: 1,
+        title: '도전과제1',
+        description: '도전과제1 상세 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
+        coin: 12,
+        progress: 50,
+    },
+    {
+        id: 2,
+        title: '도전과제2',
+        description: '도전과제2 상세 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
+        coin: 24,
+        progress: 75,
+    },
+    {
+        id: 3,
+        title: '도전과제3',
+        description: '도전과제3 상세 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
+        coin: 36,
+        progress: 20,
+    },
+    {
+        id: 4,
+        title: '도전과제4',
+        description: '도전과제4 상세 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
+        coin: 40,
+        progress: 40,
+    },
+];
+
+export const alwaysChallengeData: ChallengeData[] = [
+    {
+        id: 5,
+        title: '도전과제5',
+        description: '도전과제5 상세 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
+        coin: 50,
+        progress: 10,
+    },
+    {
+        id: 6,
+        title: '도전과제6',
+        description: '도전과제6 상세 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
+        coin: 100,
+        progress: 2,
+    },
+];
---
+++ src/modules/challenge/styles/ChallengeScreen.ts
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+import { BACKGROUND_COLORS, TEXT_COLORS } from '../../../constants/colors';
+import { TYPOGRAPHY } from '../../../constants/typography';
+
+export const styles = StyleSheet.create({
+    safeAreaView: {
+        flex: 1,
+        backgroundColor: BACKGROUND_COLORS.BACKGROUND,
+    },
+    container: {
+        paddingVertical: 16,
+        paddingHorizontal: 16,
+        gap: 24,
+    },
+    section: {
+        flex: 1,
+        paddingVertical: 12,
+        paddingHorizontal: 12,
+        gap: 12,
+    },
+    nav: {
+        flexDirection: 'row',
+        gap: 12,
+    },
+    navText: {
+        color: TEXT_COLORS.CAPTION,
+        ...TYPOGRAPHY.SUBTITLE,
+    },
+    navTextActive: {
+        color: TEXT_COLORS.DEFAULT,
+        ...TYPOGRAPHY.SUBTITLE,
+    },
+});
