ea9c5db55e67868fadf5d3bfbcdf3e4da8b41b80
sosojung <sojungmoon6@gmail.com>
2025-11-10 03:55:58 +0900

    fix :: 타이포그래피 Title 추가

--- src/constants/typography.ts
+++ src/constants/typography.ts
@@ -2,6 +2,11 @@ import { FONTS } from './fonts';
 import { fontScale } from '../utils/scalers';
 
 export const TYPOGRAPHY = {
+    Title: {
+        fontSize: fontScale(48),
+        lineHeight: 62,
+        fontFamily: FONTS.BOLD,
+    },
     HEADLINE_1: {
         fontSize: fontScale(32),
         lineHeight: 40,
