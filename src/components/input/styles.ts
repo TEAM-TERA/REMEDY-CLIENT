30785201fe1310148f35cab24fe8b4f4a5b6351d
sosojung <sojungmoon6@gmail.com>
2025-11-17 23:40:22 +0900

    refector :: 바뀐 인풋 로직대로 스타일 개선

--- src/components/input/styles.ts
+++ src/components/input/styles.ts
@@ -3,6 +3,9 @@ import { scale } from "../../utils/scalers";
 import { TEXT_COLORS, UI_COLORS } from "../../constants/colors";
 
 export const styles = StyleSheet.create({
+    wrapper: {
+        width: '100%',
+    },
     container : {
         display : "flex",
         height : scale(46),
@@ -10,10 +13,29 @@ export const styles = StyleSheet.create({
         justifyContent : "center",
         gap : scale(8),
         alignSelf : "stretch",
-        backgroundColor : UI_COLORS.BACKGROUND
+        borderRadius : scale(8),
+        backgroundColor : UI_COLORS.BACKGROUND,
+        borderWidth: 1,
+        borderColor: 'transparent',
+    },
+    containerError: {
+        borderColor: '#EF4444',
+        borderWidth: 1,
     },
     text : {
         color : TEXT_COLORS.DEFAULT,
         textAlign : "left"
-    }
+    },
+    errorText: {
+        color: '#EF4444',
+        fontSize: scale(12),
+        marginTop: scale(4),
+        marginLeft: scale(4),
+    },
+    helperText: {
+        color: TEXT_COLORS.CAPTION,
+        fontSize: scale(12),
+        marginTop: scale(4),
+        marginLeft: scale(4),
+    },
 })
