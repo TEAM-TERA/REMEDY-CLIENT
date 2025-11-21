f2afba1e431d2c2483d10d4828fec22edb28a88b
sosojung <sojungmoon6@gmail.com>
2025-11-21 11:01:49 +0900

    chore :: 토글 아이콘 이름 변경

--- src/components/icon/icons/ToggleOffSvg.tsx
+++ src/components/icon/icons/ToggleOffSvg.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { TEXT_COLORS } from '../../../constants/colors';
 import Svg, { Path } from 'react-native-svg';
 
-const ArrowUpSvg = ({ width = 24, height = 24, color = TEXT_COLORS.DEFAULT }) => {
+const ToggleOffSvg = ({ width = 24, height = 24, color = TEXT_COLORS.DEFAULT }) => {
     return (
         <Svg width={width} height={height} viewBox="0 0 24 24" fill="none">
             <Path
@@ -16,4 +16,4 @@ const ArrowUpSvg = ({ width = 24, height = 24, color = TEXT_COLORS.DEFAULT }) =>
     );
 };
 
-export default ArrowUpSvg;
+export default ToggleOffSvg;
